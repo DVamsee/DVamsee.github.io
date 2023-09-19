cd688954d8b56d3a8ad8a4097492a4a507f3179b
Vamsee D <dv26460105@gmail.com>
2023-09-19 10:17:39 +0530

    shadow updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: ["./*.{html,js}","./dist/*.{html,js}"],
   theme: {
     extend: {
+      textShadow: {
+        'neon': '0 0 10px #00ff00, 0 0 20px #00ff00, 0 0 30px #00ff00, 0 0 40px #00ff00',
+      },
     },
   },
-  plugins: [],
+  variants: {},
+  plugins: [
+    require('tailwindcss-textshadow')
+  ],
 }
 
